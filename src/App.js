1be4f8f1b1323aa8a5810e04c3c9369c1b8b4eb3
MJ-N-J <mrituanjay2003@gmail.com>
2022-07-12 12:28:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,303 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css"></link>
+
+<section id="team">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                  <h2>Meet Our Team</h2>
+                  <p></p>
+                </div>
+            </div>
+            <div class="row1">
+                <div class="col-lg-3 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="img.jpg" class="img-fluid" alt=""></img>
+                      </div>
+                        <div class="member-info">
+                            <h4>Exbo</h4>
+                            <span>Web Designer</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-github"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="img.jpg" class="img-fluid" alt=""></img>
+                      </div>
+                        <div class="member-info">
+                            <h4>Exbo</h4>
+                            <span>Web Developer</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-github"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>    
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="img.jpg" class="img-fluid" alt=""></img>
+                      </div>
+                        <div class="member-info">
+                            <h4>Exbo</h4>
+                            <span>SEO Expert</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-github"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="img.jpg" class="img-fluid" alt=""></img>
+                      </div>
+                        <div class="member-info">
+                            <h4>Exbo</h4>
+                            <span>ISO Developer</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-github"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row2">
+                <div class="col-lg-3 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="img.jpg" class="img-fluid" alt=""></img>
+                      </div>
+                        <div class="member-info">
+                            <h4>Exbo</h4>
+                            <span>Web Designer</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-github"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="img.jpg" class="img-fluid" alt=""></img>
+                      </div>
+                        <div class="member-info">
+                            <h4>Exbo</h4>
+                            <span>Web Developer</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-github"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="img.jpg" class="img-fluid" alt=""></img>
+                      </div>
+                        <div class="member-info">
+                            <h4>Exbo</h4>
+                            <span>SEO Expert</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-github"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>                  
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="img.jpg" class="img-fluid" alt=""></img>
+                      </div>
+                        <div class="member-info">
+                            <h4>Exbo</h4>
+                            <span>ISO Developer</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-github"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row3">
+                <div class="col-lg-3 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="img.jpg" class="img-fluid" alt=""></img>
+                      </div>
+                        <div class="member-info">
+                            <h4>Exbo</h4>
+                            <span>Web Designer</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-github"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="img.jpg" class="img-fluid" alt=""></img>
+                      </div>
+                        <div class="member-info">
+                            <h4>Exbo</h4>
+                            <span>Web Developer</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-github"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="img.jpg" class="img-fluid" alt=""></img>
+                      </div>
+                        <div class="member-info">
+                            <h4>Exbo</h4>
+                            <span>SEO Expert</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-github"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="img.jpg" class="img-fluid" alt=""></img>
+                      </div>
+                        <div class="member-info">
+                            <h4>Exbo</h4>
+                            <span>ISO Developer</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-github"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row4">
+                <div class="col-lg-3 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="img.jpg" class="img-fluid" alt=""></img>
+                      </div>
+                        <div class="member-info">
+                            <h4>Exbo</h4>
+                            <span>Web Designer</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-github"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="img.jpg" class="img-fluid" alt=""></img>
+                      </div>
+                        <div class="member-info">
+                            <h4>Exbo</h4>
+                            <span>Web Developer</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-github"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="img.jpg" class="img-fluid" alt=""></img>
+                      </div>
+                        <div class="member-info">
+                            <h4>Exbo</h4>
+                            <span>SEO Expert</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-github"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="member">
+                      <div class="member-img">
+                          <img src="img.jpg" class="img-fluid" alt=""></img>
+                      </div>
+                        <div class="member-info">
+                            <h4>Exbo</h4>
+                            <span>ISO Developer</span>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-github"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    
+ 
+    
+    </div>
+  );
+}
+
+export default App;
